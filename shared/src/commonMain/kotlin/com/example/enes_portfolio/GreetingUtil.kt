dd1f7022c1e7a46949964fe44e1ca5110c7d3453
enes selçuk <enesselcu25@gmail.com>
2026-08-24 16:49:21 +0300

    checkpoint before checking out main

---
+++ shared/src/commonMain/kotlin/com/example/enes_portfolio/GreetingUtil.kt
@@ -0,0 +1,4 @@
+package com.example.enes_portfolio
+
+fun sayHello(to: String): String =
+    "Hello, $to!"
---
+++ webApp/src/webMain/kotlin/com/example/enes_portfolio/main.kt
@@ -0,0 +1,11 @@
+package com.example.enes_portfolio
+
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.ComposeViewport
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() {
+    ComposeViewport {
+        App()
+    }
+}
